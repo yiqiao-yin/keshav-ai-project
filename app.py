da9d0f22d3e5f6d6e62a4d13b8988733af9ed5de
Yiqiao Yin <eagle0504@gmail.com>
2024-02-10 15:36:11 -0500

    app.py updated

--- app.py
+++ app.py
@@ -1,37 +1,34 @@
-import streamlit as st
-
-import openai
-# from openai import OpenAI
-
-# client = OpenAI(api_key = st.secrets["OPENAI_API_KEY"])
-
-# from langchain.agents import load_tools
-# from langchain.agents import initialize_agent
-# from langchain.agents import AgentType
-# from langchain.llms import OpenAI as Langchain_OpenAI
-
-import numpy as np
-import pandas as pd
-
-from scipy.spatial.distance import cosine
-
 from typing import Dict, List, Union
 
+import numpy as np
+import openai
+import pandas as pd
+import streamlit as st
 from langchain.document_loaders import TextLoader
-from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.embeddings.sentence_transformer import \
+    SentenceTransformerEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
+from scipy.spatial.distance import cosine
+
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-def merge_dataframes(dataframes):
+
+def merge_dataframes(dataframes: List[pd.DataFrame]) -> pd.DataFrame:
+    """Merges a list of DataFrames, keeping only specific columns."""
     # Concatenate the list of dataframes
-    combined_dataframe = pd.concat(dataframes, ignore_index=True)
+    combined_dataframe = pd.concat(
+        dataframes, ignore_index=True
+    )  # Combine all dataframes into one
 
     # Ensure that the resulting dataframe only contains the columns "context", "questions", "answers"
-    combined_dataframe = combined_dataframe[['context', 'questions', 'answers']]
+    combined_dataframe = combined_dataframe[
+        ["context", "questions", "answers"]
+    ]  # Filter for specific columns
 
-    return combined_dataframe
+    return combined_dataframe  # Return the merged and filtered DataFrame
+
 
 def call_chatgpt(prompt: str) -> str:
     """
@@ -62,24 +59,12 @@ def call_chatgpt(prompt: str) -> str:
     # Return the generated AI response.
     return ans
 
-# SERPAPI_API_KEY = st.secrets["SERPAPI_API_KEY"]
-
-# def call_langchain(prompt: str) -> str:
-#     llm = Langchain_OpenAI(temperature=0)
-#     tools = load_tools(["serpapi", "llm-math"], llm=llm)
-#     agent = initialize_agent(
-#         tools,
-#         llm,
-#         agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-#         verbose=True)
-#     output = agent.run(prompt)
-
-#     return output
 
 def openai_text_embedding(prompt: str) -> str:
     return openai.Embedding.create(input=prompt, model="text-embedding-ada-002")[
         "data"
     ][0]["embedding"]
+
 
 def calculate_sts_openai_score(sentence1: str, sentence2: str) -> float:
     # Compute sentence embeddings
@@ -95,17 +80,18 @@ def calculate_sts_openai_score(sentence1: str, sentence2: str) -> float:
 
     return similarity_score
 
+
 def add_dist_score_column(
-    dataframe: pd.DataFrame, sentence: str,
+    dataframe: pd.DataFrame,
+    sentence: str,
 ) -> pd.DataFrame:
     dataframe["stsopenai"] = dataframe["questions"].apply(
-            lambda x: calculate_sts_openai_score(str(x), sentence)
+        lambda x: calculate_sts_openai_score(str(x), sentence)
     )
-    
+
     sorted_dataframe = dataframe.sort_values(by="stsopenai", ascending=False)
-
-
     return sorted_dataframe.iloc[:5, :]
+
 
 def convert_to_list_of_dict(df: pd.DataFrame) -> List[Dict[str, str]]:
     """
@@ -134,161 +120,144 @@ def convert_to_list_of_dict(df: pd.DataFrame) -> List[Dict[str, str]]:
     # Return the list of dictionaries
     return result
 
-# read all csvs
-# About_YSA = pd.read_csv("YSA_CSVS/About_YSA.csv")
-# Board_of_Directors = pd.read_csv("YSA_CSVS/Board_of_Directors.csv")
-# Definition_Of_Homeless = pd.read_csv("YSA_CSVS/Definition_Of_Homeless.csv")
-# Our_Team = pd.read_csv("YSA_CSVS/Our_Team.csv")
-# Programs = pd.read_csv("YSA_CSVS/Programs.csv")
-# Tiny_House_Village_Application_Process = pd.read_csv("YSA_CSVS/Tiny_House_Village_Application_Process.csv")
-# Tiny_House_Village_Overview = pd.read_csv("YSA_CSVS/Tiny_House_Village_Overview.csv")
-# Tiny_House_Village = pd.read_csv("YSA_CSVS/Tiny_House_Village.csv")
-# Youth_Leaders_Examples = pd.read_csv("YSA_CSVS/Youth_Leaders_Examples.csv")
-# Youth_Leaders_Overview = pd.read_csv("YSA_CSVS/Youth_Leaders_Overview.csv")
-# YSA_Supporters_Lists = pd.read_csv("YSA_CSVS/YSA_Supporters_Lists.csv")
-# YSA_Supporters_Overview = pd.read_csv("YSA_CSVS/YSA_Supporters_Overview.csv")
 
-# df = merge_dataframes([About_YSA, Board_of_Directors, Definition_Of_Homeless, Our_Team, Programs, Tiny_House_Village_Application_Process,
-# Tiny_House_Village_Overview, Tiny_House_Village, Youth_Leaders_Examples, Youth_Leaders_Overview, YSA_Supporters_Lists,
-# YSA_Supporters_Overview])
-
-# doc_names = {'About_YSA': 7, 'Definition_Of_Homeless': 5, 'Our_Team': 2, 'Programs': 7, 'Tiny_House_Village_Application_Process': 5, 'Tiny_House_Village_Overview': 17, 'Tiny_House_Village': 8, 'Youth_Leaders_Examples': 11, 'Youth_Leaders_Overview': 1, 'YSA_Supporters_Lists': 10, 'YSA_Supporters_Overview': 1}
-
-file_names = ['file_0.txt',
- 'file_1.txt',
- 'file_2.txt',
- 'file_3.txt',
- 'file_4.txt',
- 'file_5.txt',
- 'file_6.txt',
- 'file_7.txt',
- 'file_8.txt',
- 'file_9.txt',
- 'file_10.txt',
- 'file_11.txt',
- 'file_12.txt',
- 'file_13.txt',
- 'file_14.txt',
- 'file_15.txt',
- 'file_16.txt',
- 'file_17.txt',
- 'file_18.txt',
- 'file_19.txt',
- 'file_20.txt',
- 'file_21.txt',
- 'file_22.txt',
- 'file_23.txt',
- 'file_24.txt',
- 'file_25.txt',
- 'file_26.txt',
- 'file_27.txt',
- 'file_28.txt',
- 'file_29.txt',
- 'file_30.txt',
- 'file_31.txt',
- 'file_32.txt',
- 'file_33.txt',
- 'file_34.txt',
- 'file_35.txt',
- 'file_36.txt',
- 'file_37.txt',
- 'file_38.txt',
- 'file_39.txt',
- 'file_40.txt',
- 'file_41.txt',
- 'file_42.txt',
- 'file_43.txt',
- 'file_44.txt',
- 'file_45.txt',
- 'file_46.txt',
- 'file_47.txt',
- 'file_48.txt',
- 'file_49.txt',
- 'file_50.txt',
- 'file_51.txt',
- 'file_52.txt',
- 'file_53.txt',
- 'file_54.txt',
- 'file_55.txt',
- 'file_56.txt',
- 'file_57.txt',
- 'file_58.txt',
- 'file_59.txt',
- 'file_60.txt',
- 'file_61.txt',
- 'file_62.txt',
- 'file_63.txt',
- 'file_64.txt',
- 'file_65.txt',
- 'file_66.txt',
- 'file_67.txt',
- 'file_68.txt',
- 'file_69.txt',
- 'file_70.txt',
- 'file_71.txt',
- 'file_72.txt',
- 'file_73.txt',
- 'file_74.txt',
- 'file_75.txt',
- 'file_76.txt',
- 'file_77.txt',
- 'file_78.txt',
- 'file_79.txt',
- 'file_80.txt',
- 'file_81.txt',
- 'file_82.txt',
- 'file_83.txt',
- 'file_84.txt',
- 'file_85.txt',
- 'file_86.txt',
- 'file_87.txt',
- 'file_88.txt',
- 'file_89.txt',
- 'file_90.txt',
- 'file_91.txt',
- 'file_92.txt',
- 'file_93.txt',
- 'file_94.txt',
- 'file_95.txt',
- 'file_96.txt',
- 'file_97.txt',
- 'file_98.txt',
- 'file_99.txt',
- 'file_100.txt',
- 'file_101.txt',
- 'file_102.txt',
- 'file_103.txt',
- 'file_104.txt',
- 'file_105.txt',
- 'file_106.txt',
- 'file_107.txt',
- 'file_108.txt',
- 'file_109.txt',
- 'file_110.txt',
- 'file_111.txt',
- 'file_112.txt',
- 'file_113.txt',
- 'file_114.txt',
- 'file_115.txt',
- 'file_116.txt',
- 'file_117.txt',
- 'file_118.txt',
- 'file_119.txt',
- 'file_120.txt',
- 'file_121.txt',
- 'file_122.txt',
- 'file_123.txt',
- 'file_124.txt',
- 'file_125.txt',
- 'file_126.txt',
- 'file_127.txt',
- 'file_128.txt',
- 'file_129.txt',
- 'file_130.txt']
+file_names = [
+    "file_0.txt",
+    "file_1.txt",
+    "file_2.txt",
+    "file_3.txt",
+    "file_4.txt",
+    "file_5.txt",
+    "file_6.txt",
+    "file_7.txt",
+    "file_8.txt",
+    "file_9.txt",
+    "file_10.txt",
+    "file_11.txt",
+    "file_12.txt",
+    "file_13.txt",
+    "file_14.txt",
+    "file_15.txt",
+    "file_16.txt",
+    "file_17.txt",
+    "file_18.txt",
+    "file_19.txt",
+    "file_20.txt",
+    "file_21.txt",
+    "file_22.txt",
+    "file_23.txt",
+    "file_24.txt",
+    "file_25.txt",
+    "file_26.txt",
+    "file_27.txt",
+    "file_28.txt",
+    "file_29.txt",
+    "file_30.txt",
+    "file_31.txt",
+    "file_32.txt",
+    "file_33.txt",
+    "file_34.txt",
+    "file_35.txt",
+    "file_36.txt",
+    "file_37.txt",
+    "file_38.txt",
+    "file_39.txt",
+    "file_40.txt",
+    "file_41.txt",
+    "file_42.txt",
+    "file_43.txt",
+    "file_44.txt",
+    "file_45.txt",
+    "file_46.txt",
+    "file_47.txt",
+    "file_48.txt",
+    "file_49.txt",
+    "file_50.txt",
+    "file_51.txt",
+    "file_52.txt",
+    "file_53.txt",
+    "file_54.txt",
+    "file_55.txt",
+    "file_56.txt",
+    "file_57.txt",
+    "file_58.txt",
+    "file_59.txt",
+    "file_60.txt",
+    "file_61.txt",
+    "file_62.txt",
+    "file_63.txt",
+    "file_64.txt",
+    "file_65.txt",
+    "file_66.txt",
+    "file_67.txt",
+    "file_68.txt",
+    "file_69.txt",
+    "file_70.txt",
+    "file_71.txt",
+    "file_72.txt",
+    "file_73.txt",
+    "file_74.txt",
+    "file_75.txt",
+    "file_76.txt",
+    "file_77.txt",
+    "file_78.txt",
+    "file_79.txt",
+    "file_80.txt",
+    "file_81.txt",
+    "file_82.txt",
+    "file_83.txt",
+    "file_84.txt",
+    "file_85.txt",
+    "file_86.txt",
+    "file_87.txt",
+    "file_88.txt",
+    "file_89.txt",
+    "file_90.txt",
+    "file_91.txt",
+    "file_92.txt",
+    "file_93.txt",
+    "file_94.txt",
+    "file_95.txt",
+    "file_96.txt",
+    "file_97.txt",
+    "file_98.txt",
+    "file_99.txt",
+    "file_100.txt",
+    "file_101.txt",
+    "file_102.txt",
+    "file_103.txt",
+    "file_104.txt",
+    "file_105.txt",
+    "file_106.txt",
+    "file_107.txt",
+    "file_108.txt",
+    "file_109.txt",
+    "file_110.txt",
+    "file_111.txt",
+    "file_112.txt",
+    "file_113.txt",
+    "file_114.txt",
+    "file_115.txt",
+    "file_116.txt",
+    "file_117.txt",
+    "file_118.txt",
+    "file_119.txt",
+    "file_120.txt",
+    "file_121.txt",
+    "file_122.txt",
+    "file_123.txt",
+    "file_124.txt",
+    "file_125.txt",
+    "file_126.txt",
+    "file_127.txt",
+    "file_128.txt",
+    "file_129.txt",
+    "file_130.txt",
+]
 
 
 # Initialize an empty list to hold all documents
-all_documents = [] # this is just a copy, you don't have to use this
+all_documents = []  # this is just a copy, you don't have to use this
 
 # Iterate over each file and load its contents
 for file_name in file_names:
@@ -301,12 +270,12 @@ text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 docs = text_splitter.split_documents(all_documents)
 
 # Create the open-source embedding function
-embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+# embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+embedding_function = openai_text_embedding
 
 # Load the documents into Chroma
 db = Chroma.from_documents(docs, embedding_function)
 
-# st.write(answer)
 
 st.title("Youth Homelessness Chatbot")
 
@@ -339,24 +308,15 @@ if prompt := st.chat_input("Tell me about YSA"):
     docs_2 = db.similarity_search_with_score(question)
     docs_2_table = pd.DataFrame(
         {
-            "source": [docs_2[i][0].metadata['source'] for i in range(len(docs))],
+            "source": [docs_2[i][0].metadata["source"] for i in range(len(docs))],
             "content": [docs_2[i][0].page_content for i in range(len(docs))],
-            "distances": [docs_2[i][1] for i in range(len(docs))]
+            "distances": [docs_2[i][1] for i in range(len(docs))],
         }
     )
 
     ref_from_db_search = docs[0].page_content
+    ref_from_db_search = ["" + docs[i].page_content for i in range(len(docs))]
 
-    ref_from_db_search = ['' + docs[i].page_content for i in range(len(docs))]
-
-    # df_screened_by_dist_score = add_dist_score_column(
-    #     df, question
-    # )
-    # qa_pairs = convert_to_list_of_dict(df_screened_by_dist_score)
-
-    # ref_from_internet = call_langchain(question)
-
-    # Based on the context: {ref_from_internet}, 
     engineered_prompt = f"""
         Based on the context: {ref_from_db_search},
         answer the user question: {question}.
